2c6d9259210adbeb975c40737876937caf06f95b
husky <husky@gaycatboy.sex>
2022-03-31 23:47:14 -0700

    FUCK YEAH WE READY

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"github.com/floppydiskette/configparser"
+	"log"
+	"net"
 	"os"
+	"os/signal"
 	"regexp"
 	"strconv"
 	"strings"
+	"syscall"
+	"time"
 )
 
 /*
@@ -43,8 +48,9 @@ type User struct {
 }
 
 type Database struct {
-	Name   string
-	Tables []Table
+	Name              string
+	Tables            []Table
+	timeSinceLastSave int
 }
 
 const (
@@ -52,6 +58,7 @@ const (
 	DemandUseTable
 	DemandFindEntry
 	DemandFindEntries
+	DemandAddEntry
 	DemandSetEntry
 	DemandSetEntries
 	DemandDeleteEntry
@@ -63,11 +70,22 @@ const (
 	DemandDeleteDatabase
 	DemandDeleteUser
 	DemandLogin
+
+	// internal demands
+	DemandGetContextFromUUID
+	DemandCreateNewContext
 )
 
 type DemandType int
 
 type Demand struct {
+	TypeOfDemand      DemandType
+	Data              interface{}
+	ReturnChannel     chan interface{}
+	AssociatedContext *Context
+}
+
+type InternalDemand struct {
 	TypeOfDemand  DemandType
 	Data          interface{}
 	ReturnChannel chan interface{}
@@ -82,6 +100,8 @@ type Context struct {
 
 var config map[string]interface{}
 
+const secondsBetweenAutoSave int = 60
+
 var dbs []Database
 var contexts []Context
 
@@ -89,15 +109,17 @@ func setup() {
 	// if os is windows, get config from C:\fuqldb\config.conf
 	// if os is linux, get config from /etc/fuqldb/config.conf
 	if os.Getenv("OS") == "Windows_NT" {
-		config, err := configparser.LoadConfig("C:\\fuqldb\\config.conf")
+		tmpConfig, err := configparser.LoadConfig("C:\\fuqldb\\config.conf")
 		if err != nil {
 			panic(err)
 		}
+		config = tmpConfig
 	} else {
-		config, err := configparser.LoadConfig("/etc/fuqldb/config.conf")
+		tmpConfig, err := configparser.LoadConfig("/etc/fuqldb/config.conf")
 		if err != nil {
 			panic(err)
 		}
+		config = tmpConfig
 	}
 
 	// make sure the config file has the right keys
@@ -109,6 +131,20 @@ func setup() {
 	}
 	if _, ok := config["sex_number"]; !ok {
 		panic("config file is missing sex_number key")
+	}
+
+	// load databases from storage path
+	files, err := os.ReadDir(config["database_storage_path"].(string))
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		newDatabase, err := loadDB(file.Name())
+		if err != nil {
+			panic(err)
+		}
+		newDatabase.timeSinceLastSave = 0
+		dbs = append(dbs, newDatabase)
 	}
 }
 
@@ -139,27 +175,29 @@ func initDB(name string, storagePath string, sex int) error {
 func serializeDB(db Database) string {
 	var out string
 	for _, table := range db.Tables {
+		// escape :
+		table.Name = strings.Replace(table.Name, ":", "\\:", -1)
 		out += fmt.Sprintf("%s:\n", table.Name)
 		for _, entry := range table.Data {
-			out += fmt.Sprintf("\t%v: %v\n", entry.Key, entry.Value)
+			// escape :
+			key := strings.Replace(fmt.Sprintf("%v", entry.Key), ":", "\\:", -1)
+			value := strings.Replace(fmt.Sprintf("%v", entry.Value), ":", "\\:", -1)
+			out += fmt.Sprintf("%s:%s\n", key, value)
 		}
 	}
 	return out
 }
 
 func parseLine(line string) (string, interface{}) {
-	// split the line into key and value
+	// split on :
 	split := strings.Split(line, ":")
-	key := split[0]
-	valueStr := strings.TrimSpace(split[1])
-	// if the value is a number, convert it to an int
-	if value, err := strconv.Atoi(valueStr); err == nil {
-		return key, value
-	}
-	return key, valueStr
+	// split on \:
+	split[0] = strings.Replace(split[0], "\\:", ":", -1)
+	split[1] = strings.Replace(split[1], "\\:", ":", -1)
+	return split[0], split[1]
 }
 
-func loadDB(inFile string) Database {
+func loadDB(inFile string) (Database, error) {
 	var db Database
 	file, err := os.Open(inFile)
 	if err != nil {
@@ -170,13 +208,13 @@ func loadDB(inFile string) Database {
 		var line string
 		_, err := fmt.Fscanln(file, &line)
 		if err != nil {
-			break
+			return db, err
 		}
 		table.Name = line
 		for {
 			_, err := fmt.Fscanln(file, &line)
 			if err != nil {
-				break
+				return db, err
 			}
 			if line == "" {
 				break
@@ -187,7 +225,36 @@ func loadDB(inFile string) Database {
 		}
 		db.Tables = append(db.Tables, table)
 	}
-	return db
+	return db, nil
+}
+
+func (db *Database) saveDB(outFile string) error {
+	file, err := os.Create(outFile)
+	if err != nil {
+		return err
+	}
+	for _, table := range db.Tables {
+		// escape :
+		table.Name = strings.Replace(table.Name, ":", "\\:", -1)
+		_, err := fmt.Fprintf(file, "%s:\n", table.Name)
+		if err != nil {
+			return err
+		}
+		for _, entry := range table.Data {
+			// escape :
+			key := strings.Replace(fmt.Sprintf("%v", entry.Key), ":", "\\:", -1)
+			value := strings.Replace(fmt.Sprintf("%v", entry.Value), ":", "\\:", -1)
+			_, err := fmt.Fprintf(file, "%s:%s\n", key, value)
+			if err != nil {
+				return err
+			}
+		}
+		_, err = fmt.Fprintf(file, "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (db *Database) getTable(name string) *Table {
@@ -692,6 +759,14 @@ func (ctx *Context) demandHandler(d Demand) (interface{}, error) {
 			return nil, errors.New("demand data is not a string")
 		}
 		ctx.addTable(d.Data.(string))
+	case DemandAddEntry:
+		// make sure that the data of the demand is a string (key,value)
+		if _, ok := d.Data.(string); !ok {
+			return nil, errors.New("demand data is not a string")
+		}
+		key := strings.Split(d.Data.(string), ",")[0]
+		value := strings.Split(d.Data.(string), ",")[1]
+		ctx.addEntry(key, value)
 	case DemandSetEntry:
 		// make sure that the data of the demand is a string array (the key and value of the entry)
 		if _, ok := d.Data.([]interface{}); !ok {
@@ -882,6 +957,264 @@ func (ctx *Context) demandHandler(d Demand) (interface{}, error) {
 	return nil, nil
 }
 
+func (ctx *Context) parseCommand(cmd string) (*Demand, error) {
+	var d *Demand
+	if cmd == "" {
+		return nil, errors.New("command is empty")
+	}
+	insideString := false
+	var commandArray []string
+	// go through command and split by spaces unless it's a string
+	// if it's a string, split by "
+	for i, c := range cmd {
+		if c == '"' {
+			if i == 0 || cmd[i-1] != '\\' {
+				insideString = !insideString
+			}
+		}
+		if c == ' ' && !insideString {
+			commandArray = append(commandArray, cmd[:i])
+			cmd = cmd[i+1:]
+			i = 0
+		}
+	}
+	switch strings.ToLower(commandArray[0]) {
+	case "use":
+		switch strings.ToLower(commandArray[1]) {
+		case "database":
+			d.TypeOfDemand = DemandUseDatabase
+			d.Data = commandArray[2]
+		case "table":
+			d.TypeOfDemand = DemandUseTable
+			d.Data = commandArray[2]
+		default:
+			return nil, errors.New("unknown use command")
+		}
+	case "login":
+		d.TypeOfDemand = DemandLogin
+		d.Data = commandArray[1:]
+	case "tell":
+		switch strings.ToLower(commandArray[1]) {
+		case "entry":
+			switch strings.ToLower(commandArray[2]) {
+			case "to":
+				switch strings.ToLower(commandArray[3]) {
+				case "present":
+					// if next word is "where", then it will be finding entries
+					// otherwise, the next word will be the key
+					if strings.ToLower(commandArray[4]) == "where" {
+						// if next word is "key", then find entries by key
+						// otherwise, find entries by value
+						if strings.ToLower(commandArray[5]) == "key" {
+							d.TypeOfDemand = DemandFindEntries
+							d.Data = []interface{}{true, commandArray[6]}
+						} else if strings.ToLower(commandArray[5]) == "value" {
+							d.TypeOfDemand = DemandFindEntries
+							d.Data = []interface{}{false, commandArray[6]}
+						} else {
+							return nil, errors.New("unknown tell entry to present command")
+						}
+					} else {
+						d.TypeOfDemand = DemandFindEntry
+						// last word will be the key
+						d.Data = commandArray[4]
+					}
+				case "create":
+					// first word is key,value
+					d.TypeOfDemand = DemandAddEntry
+					d.Data = commandArray[4]
+				case "fuck":
+					// if next word is off, tell entry to fuck off
+					if strings.ToLower(commandArray[4]) == "off" {
+						// if next word is "where", then it will be finding entries
+						// otherwise, the next word will be the key
+						if strings.ToLower(commandArray[5]) == "where" {
+							// if next word is "key", then find entries by key
+							// otherwise, find entries by value
+							if strings.ToLower(commandArray[6]) == "key" {
+								d.TypeOfDemand = DemandDeleteEntries
+								d.Data = []interface{}{true, commandArray[7]}
+							} else if strings.ToLower(commandArray[6]) == "value" {
+								d.TypeOfDemand = DemandDeleteEntries
+								d.Data = []interface{}{false, commandArray[7]}
+							} else {
+								return nil, errors.New("unknown tell entry to fuck off command")
+							}
+						} else {
+							d.TypeOfDemand = DemandDeleteEntry
+							// last word will be the key
+							d.Data = commandArray[5]
+						}
+					} else {
+						return nil, errors.New("unknown tell entry to fuck command")
+					}
+				case "become":
+					// if next word is "where", then it will be finding entries
+					// otherwise, the next word will be the key
+					if strings.ToLower(commandArray[4]) == "where" {
+						// if next word is "key", then find entries by key
+						// otherwise, find entries by value
+						if strings.ToLower(commandArray[5]) == "key" {
+							d.TypeOfDemand = DemandSetEntries
+							d.Data = []interface{}{true, commandArray[6]}
+						} else if strings.ToLower(commandArray[5]) == "value" {
+							d.TypeOfDemand = DemandSetEntries
+							d.Data = []interface{}{false, commandArray[6]}
+						} else {
+							return nil, errors.New("unknown tell entry to become command")
+						}
+					} else {
+						d.TypeOfDemand = DemandSetEntry
+						// last word will be the key and value separated by a comma
+						d.Data = strings.Split(commandArray[4], ",")
+					}
+				}
+			}
+		case "table":
+			switch strings.ToLower(commandArray[2]) {
+			case "to":
+				switch strings.ToLower(commandArray[3]) {
+				case "create":
+					// first word is name of table
+					d.TypeOfDemand = DemandCreateTable
+					d.Data = commandArray[4]
+				case "fuck":
+					// if next word is off, tell table to fuck off
+					if strings.ToLower(commandArray[4]) == "off" {
+						d.TypeOfDemand = DemandDeleteTable
+						// last word will be the name of the table
+						d.Data = commandArray[5]
+					} else {
+						return nil, errors.New("unknown tell table to fuck command")
+					}
+				}
+			}
+		case "database":
+			switch strings.ToLower(commandArray[2]) {
+			case "to":
+				switch strings.ToLower(commandArray[3]) {
+				case "create":
+					// first word is name of database
+					d.TypeOfDemand = DemandCreateDatabase
+					d.Data = commandArray[4]
+				case "fuck":
+					// if next word is off, tell database to fuck off
+					if strings.ToLower(commandArray[4]) == "off" {
+						d.TypeOfDemand = DemandDeleteDatabase
+						// last word will be the name of the database
+						d.Data = commandArray[5]
+					} else {
+						return nil, errors.New("unknown tell database to fuck command")
+					}
+				}
+			}
+		}
+	}
+
+	return d, nil
+}
+
+func handleConnection(conn net.Conn, commandChannel chan *Demand) {
+	defer conn.Close()
+	for {
+		// read until a null byte
+		buf := make([]byte, 4096)
+		for {
+			_, err := conn.Read(buf)
+			if err != nil {
+				return
+			}
+			if buf[0] == 0 {
+				break
+			}
+		}
+		// get uuid (first 36 bytes)
+		uuid := make([]byte, 36)
+		copy(uuid, buf[:36])
+		// if all are 0s, the client wants to create a new context
+		// send that back as a demand
+		allZeros := true
+		for _, char := range uuid {
+			if char != '0' {
+				allZeros = false
+			}
+		}
+		if allZeros == true {
+			var returnChannel chan interface{}
+			newDemand := &Demand{
+				TypeOfDemand:  DemandCreateNewContext,
+				Data:          nil,
+				ReturnChannel: returnChannel,
+			}
+			commandChannel <- newDemand
+
+			// await response
+			response := <-returnChannel
+			// send response to client
+			_, err := conn.Write([]byte(response.(string)))
+			if err != nil {
+				fmt.Println("WARNING: error writing to client: ", err)
+			}
+		} else {
+			// demand to find context by uuid
+			var returnChannel chan interface{}
+			newDemand := &Demand{
+				TypeOfDemand:  DemandGetContextFromUUID,
+				Data:          uuid,
+				ReturnChannel: returnChannel,
+			}
+			commandChannel <- newDemand
+
+			// await response
+			response := <-returnChannel
+			// response should be a context pointer, check
+			if response == nil {
+				fmt.Println("WARNING: response from demand channel was nil")
+				continue
+			} else if response.(*Context) == nil {
+				fmt.Println("WARNING: response from demand channel was not a context pointer")
+				continue
+			} else {
+				// get the command (rest of the bytes)
+				command := make([]byte, len(buf)-36)
+				copy(command, buf[36:])
+				// parse the command
+				newDemand, err := response.(*Context).parseCommand(string(command))
+				if err != nil {
+					fmt.Println("WARNING: error parsing command: ", err)
+					continue
+				}
+				// create a return channel and add it to the demand
+				newDemand.ReturnChannel = make(chan interface{})
+				// assign the context to the demand
+				newDemand.AssociatedContext = response.(*Context)
+				// send the demand to the command channel
+				commandChannel <- newDemand
+				// await response
+				response = <-newDemand.ReturnChannel
+				// send response to client
+				_, err = conn.Write([]byte(response.(string)))
+			}
+		}
+	}
+}
+
+func handleServer(commandChannel chan *Demand) {
+	// listen on port 8008 for commands
+	listener, err := net.Listen("tcp", ":8008")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn, commandChannel)
+	}
+}
+
 func main() {
 	// if arg --init is passed, initialize the database
 	if len(os.Args) > 1+3 && os.Args[1] == "--init" {
@@ -894,6 +1227,68 @@ func main() {
 			panic(err)
 		}
 		return
+	} else {
+		setup()
 	}
 
+	// load each database file from the database storage path
+	files, err := os.ReadDir(config["database_storage_path"].(string))
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		newDatabase, err := loadDB(file.Name())
+		if err != nil {
+			panic(err)
+		}
+		newDatabase.timeSinceLastSave = 0
+		dbs = append(dbs, newDatabase)
+	}
+
+	var slowDown int64 = 0
+
+	sigs := make(chan os.Signal, 10)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	// create a command channel
+	commandChannel := make(chan *Demand, 1000)
+	go handleServer(commandChannel)
+	for {
+		if len(commandChannel) > 0 {
+			newDemand := <-commandChannel
+			if newDemand.AssociatedContext != nil {
+				dataBack, err := newDemand.AssociatedContext.demandHandler(*newDemand)
+				if err != nil {
+					fmt.Println("WARNING: error handling demand: ", err)
+				}
+				if newDemand.ReturnChannel != nil {
+					newDemand.ReturnChannel <- dataBack
+				}
+			}
+		}
+		if time.Now().Unix() > slowDown-30 {
+			slowDown = time.Now().Unix()
+			for _, db := range dbs {
+				if time.Now().Unix()-int64(db.timeSinceLastSave) > int64(secondsBetweenAutoSave) {
+					err := db.saveDB(config["database_storage_path"].(string) + db.Name + ".db")
+					if err != nil {
+						fmt.Println("WARNING: error saving database: ", err)
+					}
+				}
+			}
+		}
+		// check for kill signals
+		if len(sigs) > 0 {
+			sig := <-sigs
+			// save databases
+			for _, db := range dbs {
+				err := db.saveDB(config["database_storage_path"].(string) + db.Name + ".db")
+				if err != nil {
+					fmt.Println("WARNING: error saving database: ", err)
+				}
+			}
+			fmt.Println("Killed by signal: ", sig)
+			os.Exit(0)
+		}
+	}
 }
